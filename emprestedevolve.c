e255054d05ff5b1c433a29ebaeb19dacb8b4a8ce
Dassa1 <hadassajuliane405@gmail.com>
2026-04-06 13:56:44 -0300

    Add files via upload

---
+++ emprestedevolve.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char titulo[50];
+    int status; // 0 = emprestado | 1 = disponível
+} Livro;
+
+// Exemplo com 2 livros já cadastrados
+Livro biblioteca[] = {
+    {"Coraline", 1},
+    {"O pequeno principe", 1}
+};
+
+int totalLivros = 2;
+
+// Função para emprestar
+void emprestarLivro() {
+    char nome[50];
+    printf("Digite o nome do livro para emprestar: ");
+    scanf(" %[^\n]", nome);
+
+    for(int i = 0; i < totalLivros; i++) {
+        if(strcmp(nome, biblioteca[i].titulo) == 0) {
+
+            if(biblioteca[i].status == 0) {
+                printf("Livro ja esta emprestado!\n");
+            } else {
+                biblioteca[i].status = 0;
+                printf("Emprestimo realizado com sucesso!\n");
+            }
+            return;
+        }
+    }
+
+    printf("Livro nao encontrado!\n");
+}
+
+// Função para devolver
+void devolverLivro() {
+    char nome[50];
+    printf("Digite o nome do livro para devolver: ");
+    scanf(" %[^\n]", nome);
+
+    for(int i = 0; i < totalLivros; i++) {
+        if(strcmp(nome, biblioteca[i].titulo) == 0) {
+
+            if(biblioteca[i].status == 1) {
+                printf("Livro ja esta disponivel!\n");
+            } else {
+                biblioteca[i].status = 1;
+                printf("Devolucao realizada com sucesso!\n");
+            }
+            return;
+        }
+    }
+
+    printf("Livro nao encontrado!\n");
+}
+
+// MAIN simples só pra testar
+int main() {
+    emprestarLivro();
+    devolverLivro();
+    return 0;
+}
